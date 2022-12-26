742d5a40b1360b89bf9cb040c5adac52ab13b105
Talion426 <106770432+Talion426@users.noreply.github.com>
2022-12-26 19:34:56 +0300

    fix (components): add responsive to account

--- src/components/Account/styles.ts
+++ src/components/Account/styles.ts
@@ -1,21 +1,31 @@
 import styled from "styled-components";
-import { Color, Typography } from "ui";
+import { Color, Typography, Media } from "ui";
 
 export const ProfileWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   column-gap: 32px;
+
   margin-bottom: 48px;
+
+  ${Media.MD} {
+    grid-template-columns: 1fr;
+  }
 `;
 
 export const PasswordWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   column-gap: 32px;
+
+  ${Media.MD} {
+    grid-template-columns: 1fr;
+  }
 `;
 
 export const Subtitle = styled.h3`
   margin-bottom: 16px;
+
   ${Typography.H2}
   color: ${Color.Primary};
 `;
@@ -24,7 +34,9 @@ export const ButtonsWrapper = styled.div`
   display: flex;
   justify-content: end;
   gap: 30px;
+
   padding-top: 48px;
   margin: 72px 0;
+
   border-top: 1px solid ${Color.Gray};
 `;
