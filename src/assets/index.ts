16619c0e1bee702b0b99c2eab760ce447b6c8052
Talion426 <106770432+Talion426@users.noreply.github.com>
2022-12-26 15:09:57 +0300

    fix (assets): rename icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,13 +1,13 @@
-import { ReactComponent as LogoIcon } from "./icons/bookstore-logo-icon.svg";
-import { ReactComponent as HeartIcon } from "./icons/heart-icon.svg";
-import { ReactComponent as ShoppingBagIcon } from "./icons/shopping-bag-icon.svg";
-import { ReactComponent as AccountIcon } from "./icons/account-icon.svg";
-import { ReactComponent as BurgerMenuIcon } from "./icons/burger-menu-icon.svg";
-import { ReactComponent as SearchIcon } from "./icons/search-icon.svg";
-import { ReactComponent as CancelIcon } from "./icons/cancel-icon.svg";
-import { ReactComponent as BackArrowIcon } from "./icons/back-arrow-icon.svg";
-import { ReactComponent as PrevArrowIcon } from "./icons/prev-arrow-icon.svg";
-import { ReactComponent as NextArrowIcon } from "./icons/next-arrow-icon.svg";
+import { ReactComponent as LogoIcon } from "./icons/bookstore-logo.svg";
+import { ReactComponent as HeartIcon } from "./icons/heart.svg";
+import { ReactComponent as ShoppingBagIcon } from "./icons/shopping-bag.svg";
+import { ReactComponent as AccountIcon } from "./icons/account.svg";
+import { ReactComponent as BurgerMenuIcon } from "./icons/burger-menu.svg";
+import { ReactComponent as SearchIcon } from "./icons/search.svg";
+import { ReactComponent as CancelIcon } from "./icons/cancel.svg";
+import { ReactComponent as BackArrowIcon } from "./icons/back-arrow.svg";
+import { ReactComponent as PrevArrowIcon } from "./icons/prev-arrow.svg";
+import { ReactComponent as NextArrowIcon } from "./icons/next-arrow.svg";
 
 export {
   LogoIcon,
