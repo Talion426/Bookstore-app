8953e65c46756fa49be8cce07f99542e3b4e8082
Talion426 <106770432+Talion426@users.noreply.github.com>
2022-12-27 16:09:46 +0300

    feat (pages): ad content to search page

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,3 +1,26 @@
+import { BooksList, Title } from "components";
+import { useEffect } from "react";
+import { fetchNewBooks, getNewBooks, useAppDispatch, useAppSelector } from "store";
+import { SearchBooksWrapper, Subtitle } from "./styles";
+
 export const SearchPage = () => {
-  return <div>SearchPage</div>;
+  const { isLoading, result } = useAppSelector(getNewBooks);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(fetchNewBooks());
+  }, [dispatch]);
+
+  return (
+    <section>
+      {isLoading ? (
+        <Title text="Loading..." />
+      ) : (
+        <SearchBooksWrapper>
+          <Title text="' ' Search result" />
+          <Subtitle>{`Found ${result.length} books`}</Subtitle>
+          <BooksList responseBooks={result} />
+        </SearchBooksWrapper>
+      )}
+    </section>
+  );
 };
--- src/pages/SearchPage/styles.ts
+++ src/pages/SearchPage/styles.ts
@@ -1,1 +1,12 @@
 import styled from "styled-components";
+import { Color, Typography } from "ui";
+
+export const SearchBooksWrapper = styled.section`
+  margin-bottom: 72px;
+`;
+
+export const Subtitle = styled.p`
+  margin-bottom: 48px;
+  ${Typography.B1};
+  color: ${Color.Secondary};
+`;
