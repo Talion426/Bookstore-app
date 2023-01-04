3cdf439600e184d0a98e44624444a7771fd062df
Talion426 <106770432+Talion426@users.noreply.github.com>
2023-01-04 16:36:14 +0300

    feat (components): add pagination to search page

--- src/components/BooksList/BooksList.tsx
+++ src/components/BooksList/BooksList.tsx
@@ -1,42 +1,19 @@
 import { BookListItem, Pagination } from "components";
 import { IBook } from "types";
 import { StyledBooksList } from "./styles";
-import { useEffect, useState } from "react";
 
 interface IProps {
   books: IBook[];
 }
 
 export const BooksList = ({ books }: IProps) => {
-  const [currentItems, setCurrentItems] = useState<IBook[]>([]);
-  const [pageCount, setPageCount] = useState<number>(0);
-  const [itemOffset, setItemOffset] = useState<number>(0);
-  const ITEMS_PER_PAGE = 9;
-
-  useEffect(() => {
-    const endOffset = itemOffset + ITEMS_PER_PAGE;
-    setCurrentItems(books.slice(itemOffset, endOffset));
-    setPageCount(Math.ceil(books.length / ITEMS_PER_PAGE));
-  }, [itemOffset, ITEMS_PER_PAGE, books]);
-
-  const handlePageClick = (event: { selected: number }): void => {
-    const newOffset = event.selected * ITEMS_PER_PAGE;
-    setItemOffset(newOffset);
-
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
-
   return (
     <>
       <StyledBooksList>
-        {currentItems.map((book) => (
+        {books.map((book) => (
           <BookListItem key={book.isbn13} book={book} />
         ))}
       </StyledBooksList>
-      <Pagination pageCount={pageCount} PageClick={handlePageClick} />
     </>
   );
 };
--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,26 +1,40 @@
-import { BackArrowButton, BooksList, Title } from "components";
-import { useEffect } from "react";
+import { BackArrowButton, BooksList, Pagination, Title } from "components";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchSearchBooks, getSearchBooks, useAppDispatch, useAppSelector } from "store";
 import { SearchBooksWrapper, Subtitle } from "./styles";
 
 export const SearchPage = () => {
-  const { page, searchValue } = useParams();
   const dispatch = useAppDispatch();
+  const { page, searchValue } = useParams();
+  const { result, isLoading, error } = useAppSelector(getSearchBooks);
+  const { books, total } = result;
+
+  const ITEMS_PER_PAGE = 10;
+  const pageCount = Math.ceil(+total / ITEMS_PER_PAGE);
+  const [itemOffset, setItemOffset] = useState(page);
+
+  const handlePageClick = (event: { selected: number }): void => {
+    const newOffset = event.selected;
+
+    if (newOffset) {
+      setItemOffset(String(newOffset));
+    }
+
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
   useEffect(() => {
     dispatch(
       fetchSearchBooks({
         searchValue: searchValue,
-        page: page,
+        page: itemOffset,
       }),
     );
-  }, [searchValue, page, dispatch]);
-
-  const { result, isLoading, error } = useAppSelector(getSearchBooks);
-  const { books, total } = result;
-
-  console.log(result);
+  }, [searchValue, itemOffset, dispatch]);
 
   return (
     <SearchBooksWrapper>
@@ -37,6 +51,7 @@ export const SearchPage = () => {
           <Title text={`'${searchValue}' search results`} />
           <Subtitle>{`Found ${total} books`}</Subtitle>
           {<BooksList books={books} />}
+          <Pagination pageCount={pageCount} PageClick={handlePageClick} />
         </>
       )}
     </SearchBooksWrapper>
