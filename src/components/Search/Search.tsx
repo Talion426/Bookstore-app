841aec3c6a5294d024b3d105d323e74ba8723479
Talion426 <106770432+Talion426@users.noreply.github.com>
2023-01-16 14:08:18 +0300

    fix (components): fix search

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,10 +1,7 @@
 import { SearchIcon } from "assets";
-import { SearchModal } from "components";
 import { useForm } from "react-hook-form";
-import { useWindowSize } from "hooks";
 import { useNavigate } from "react-router-dom";
 import { SearchButton, SearchWrapper, StyledSearch } from "./styles";
-import { Color } from "ui";
 
 interface IProps {
   closeBurger: () => void;
@@ -15,40 +12,31 @@ interface ISearch {
 }
 
 export const Search = ({ closeBurger }: IProps) => {
-  const { width = 0 } = useWindowSize();
   const navigate = useNavigate();
+
+  const { register, watch } = useForm({
+    defaultValues: {
+      search: "",
+    },
+  });
+
+  const value = watch("search");
 
   const handleSearchButton = () => {
     navigate(`search/${value}/1`);
   };
 
-  const { register, handleSubmit, reset, watch } = useForm({
-    defaultValues: {
-      search: "",
-    },
-    mode: "onChange",
-  });
-
-  const handleSearch = (search: ISearch) => {
-    reset();
-  };
-
-  const value = watch("search");
-
   return (
-    <SearchWrapper onSubmit={handleSubmit(handleSearch)}>
+    <SearchWrapper>
       <StyledSearch
         type="text"
         placeholder="Search"
-        value={value}
         {...register("search", {
           required: {
             value: true,
             message: "Please, enter a value",
           },
         })}
-        whileFocus={{ border: `1px solid ${Color.Primary}` }}
-        transition={{ duration: 0.3, ease: "easeInOut" }}
       />
       <SearchButton
         onClick={() => {
@@ -58,8 +46,6 @@ export const Search = ({ closeBurger }: IProps) => {
       >
         <SearchIcon />
       </SearchButton>
-
-      {width > 9992 && value && <SearchModal value={value} />}
     </SearchWrapper>
   );
 };
